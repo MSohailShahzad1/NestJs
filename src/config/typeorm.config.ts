1a43164339dea0821c16a909357bb3d7432afec1
sohail13579 <msohailshahzad13579@gmail.com>
2026-03-02 14:55:45 +0500

    Changed the Databse to PostgreSQL

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,8 +4,12 @@ import { ConfigService } from '@nestjs/config';
 export const typeOrmConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => ({
-  type: 'sqlite',
-  database: configService.get<string>('DB_NAME') ?? 'dev.sqlite',
+  type: 'postgres',
+  host: configService.get<string>('DB_HOST') ?? 'localhost',
+  port: configService.get<number>('DB_PORT') ?? 5432,
+  username: configService.get<string>('DB_USER') ?? 'postgres',
+  password: configService.get<string>('DB_PASSWORD') ?? 'postgres',
+  database: configService.get<string>('DB_NAME') ?? 'blogapp',
   autoLoadEntities: true,
   synchronize: true,
 });
